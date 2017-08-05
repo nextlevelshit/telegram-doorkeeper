2e961443b2e090c6231784d1df9980e132de16c8
Michael Czechowski <mail@dailysh.it>
2017-08-05 15:47:16 +0200

    running

---
+++ index.js
@@ -0,0 +1,82 @@
+// External libraries being loaded at the beginning
+const TelegramBot = require('node-telegram-bot-api');
+const Emoji = require('node-emoji').emoji;
+// Local libraries and configurating local constants
+const config = require('./config');
+// Configurating our main piece of code
+const bot = new TelegramBot(config.telegram.token, {polling: true});
+
+let doors = false;
+
+// bot.on('text', (msg) => {
+//   if (msg.chat.id === -180708744) {
+//     console.log('Messages tracked on Cafe Faust Offtopic');
+//   }
+
+// });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, `Hi ${msg.from.first_name}, wie kann ich dir helfen?`, {
+    'reply_markup': {
+      'keyboard': [
+        [`${Emoji.unlock} ${config.text.opened}`, `${Emoji.lock} ${config.text.closed}`], 
+        ['Ist gerade jemand unten?']]
+    }
+  })
+});
+
+bot.onText(/\/open/, (msg) => {
+  if(msg.chat.id === config.telegram.channel) {
+    open();
+  }
+});
+
+bot.onText(/Geöffnet$/, (msg) => {
+  bot.getChatMember(config.telegram.channel, msg.chat.id).then((channel) => {
+    if(channel.user.id === msg.chat.id) {
+      open();
+    }
+  });
+});
+
+bot.onText(/\/close/, (msg) => {
+  if(msg.chat.id === config.telegram.channel) {
+    close();
+  }
+});
+
+bot.onText(/Geschlossen$/, (msg) => {
+  bot.getChatMember(config.telegram.channel, msg.chat.id).then((channel) => {
+    if(channel.user.id === msg.chat.id) {
+      close();
+    }
+  });
+});
+
+bot.onText(/\/doors/, (msg) => {
+  console.log(msg.chat.first_name, 'Requesting doors status');
+
+  bot.sendMessage(msg.chat.id, (doors) ? 'Hereinspaziert! Das Faust ist offen!' : 'Sorry Bro, leider geschlossen :(');
+});
+
+bot.onText(/(?:(?:jemand|(?:da|unten|im|in))|(?:(?:unten|im|in)|faust))|(?:ist|(?:offen|geöffnet)).*?\?$/, (msg) => {
+  bot.sendMessage(msg.chat.id, (doors) ? `Klar ${msg.from.first_name}, es ist offen! Komm vorbei :)` : `Sorry ${msg.from.first_name}, gerade ist niemand da :/`);
+}); 
+
+bot.on('message', (msg) => {
+  console.log(msg.chat.id, msg.text);
+});
+
+function open() {
+  doors = true;
+  console.log('Doors opened at Café Faust');
+  return;
+}
+
+function close() {
+  doors = false;
+  console.log('Doors closed at Café Faust');
+  return;
+}
+
+// bot.onText(/\/geöffnet/)
